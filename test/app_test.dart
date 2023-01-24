b4107cf8d9f55bb696cd508602b3ea34646082c8
Orkunnnn <orkunkonak1414@gmail.com>
2023-01-24 20:23:07 +0300

    test: init hydrated bloc

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,9 +1,30 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import "package:flutter_gen/gen_l10n/app_localizations.dart";
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:movie_buddy/app.dart';
+import 'package:movie_buddy/core/l10n/cubit/localization_cubit.dart';
+import 'package:movie_buddy/core/theme/cubit/theme_cubit.dart';
+
+import 'helpers/hydrated_bloc.dart';
+
+class MockLocalizationCubit extends MockCubit<Locale?>
+    implements LocalizationCubit {}
+
+class MockThemeCubit extends MockCubit<ThemeMode> implements ThemeCubit {}
 
 void main() {
+  initHydratedStorage();
+  late ThemeCubit themeCubit;
+  late LocalizationCubit localizationCubit;
+  setUp(
+    () {
+      themeCubit = MockThemeCubit();
+      localizationCubit = MockLocalizationCubit();
+    },
+  );
   group(
     "App",
     () {
@@ -22,11 +43,25 @@ void main() {
       testWidgets(
         "renders AppBar",
         (tester) async {
+          when(
+            () => themeCubit.state,
+          ).thenReturn(ThemeMode.light);
+          when(
+            () => localizationCubit.state,
+          ).thenReturn(const Locale("tr"));
           await tester.pumpWidget(
-            const MaterialApp(
-              localizationsDelegates: AppLocalizations.localizationsDelegates,
-              supportedLocales: AppLocalizations.supportedLocales,
-              home: AppView(),
+            MultiBlocProvider(
+              providers: [
+                BlocProvider.value(
+                  value: themeCubit,
+                ),
+                BlocProvider.value(value: localizationCubit)
+              ],
+              child: const MaterialApp(
+                localizationsDelegates: AppLocalizations.localizationsDelegates,
+                supportedLocales: AppLocalizations.supportedLocales,
+                home: AppView(),
+              ),
             ),
           );
           expect(find.byType(AppBar), findsOneWidget);
@@ -38,12 +73,24 @@ void main() {
           testWidgets(
             "renders title",
             (tester) async {
+              when(
+                () => themeCubit.state,
+              ).thenReturn(ThemeMode.light);
+              when(
+                () => localizationCubit.state,
+              ).thenReturn(const Locale("en"));
               await tester.pumpWidget(
-                const MaterialApp(
-                  localizationsDelegates:
-                      AppLocalizations.localizationsDelegates,
-                  supportedLocales: AppLocalizations.supportedLocales,
-                  home: AppView(),
+                MultiBlocProvider(
+                  providers: [
+                    BlocProvider.value(value: themeCubit),
+                    BlocProvider.value(value: localizationCubit)
+                  ],
+                  child: const MaterialApp(
+                    localizationsDelegates:
+                        AppLocalizations.localizationsDelegates,
+                    supportedLocales: AppLocalizations.supportedLocales,
+                    home: AppView(),
+                  ),
                 ),
               );
               expect(find.text("Movie Buddy"), findsOneWidget);
