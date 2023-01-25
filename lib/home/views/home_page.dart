0b37175be4a1bcdeba8690fb9ce5ef25b8a61d8a
Orkunnnn <orkunkonak1414@gmail.com>
2023-01-25 13:54:57 +0300

    feat: add search icon

--- lib/home/views/home_page.dart
+++ lib/home/views/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ionicons/ionicons.dart';
 import 'package:movie_buddy/l10n/cubit/localization_cubit.dart';
 import 'package:movie_buddy/l10n/l10n.dart';
 import 'package:movie_buddy/settings/views/settings_page.dart';
 import 'package:movie_buddy/theme/cubit/theme_cubit.dart';
+import 'package:movie_buddy_ui/movie_buddy_ui.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,24 +13,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
-    return Scaffold(
-      appBar: AppBar(
-        actions: const [],
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(l10n.helloWorld),
-            ElevatedButton(
-              onPressed: () => Navigator.of(context).push(
-                SettingsPage.route(
-                  localizationCubit: context.read<LocalizationCubit>(),
-                  themeCubit: context.read<ThemeCubit>(),
+    return Padding(
+      padding: MovieBuddyPadding.horizontal.medium,
+      child: Scaffold(
+        appBar: AppBar(
+          actions: const [Icon(Ionicons.search)],
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text(l10n.helloWorld),
+              ElevatedButton(
+                onPressed: () => Navigator.of(context).push(
+                  SettingsPage.route(
+                    localizationCubit: context.read<LocalizationCubit>(),
+                    themeCubit: context.read<ThemeCubit>(),
+                  ),
                 ),
-              ),
-              child: const Text("Go to settings"),
-            )
-          ],
+                child: const Text("Go to settings"),
+              )
+            ],
+          ),
         ),
       ),
     );
