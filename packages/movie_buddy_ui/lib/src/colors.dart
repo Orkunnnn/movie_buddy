1d15d6c2616a15ea58815c308f1a50a43e6f4f83
Orkunnnn <orkunkonak1414@gmail.com>
2023-01-25 10:55:44 +0300

    chore: add app colors

---
+++ packages/movie_buddy_ui/lib/src/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+abstract class MovieBuddyColors {
+  static const Color black = Color(0xff000000);
+  static const Color yellow = Color(0xffF2F714);
+  static const Color transparent = Color(0x00000000);
+  static const Color white = Color(0xffffffff);
+  static const Color lightGrey = Color(0xff2A272A);
+}
