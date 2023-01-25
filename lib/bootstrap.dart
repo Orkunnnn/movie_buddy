5aae6340ecd504e6d41e630907b47de71d0b6a6c
Orkunnnn <orkunkonak1414@gmail.com>
2023-01-25 10:53:41 +0300

    chore: move main functions to the bootstrap

---
+++ lib/bootstrap.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: avoid_print
+
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:movie_buddy/app/app.dart';
+import 'package:movie_buddy/app/app_bloc_observer.dart';
+import 'package:path_provider/path_provider.dart';
+
+Future<void> bootstrap() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlutterError.onError = (details) {
+    print(details.exceptionAsString());
+    print(details.stack);
+  };
+  Bloc.observer = AppBlocObserver();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
+  runZonedGuarded(
+    () => runApp(const App()),
+    (error, stack) {
+      print(error);
+      print(stack);
+    },
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,5 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:movie_buddy/app.dart';
-import 'package:movie_buddy/core/bloc_observer.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:movie_buddy/bootstrap.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  Bloc.observer = AppBlocObserver();
-  HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: kIsWeb
-        ? HydratedStorage.webStorageDirectory
-        : await getTemporaryDirectory(),
-  );
-  runApp(const App());
+  await bootstrap();
 }
