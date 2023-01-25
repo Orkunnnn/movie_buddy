30080ecfaf34e74857e1d041803d1f95cab340f8
Orkunnnn <orkunkonak1414@gmail.com>
2023-01-25 10:58:02 +0300

    chore: export ui files

---
+++ packages/movie_buddy_ui/lib/movie_buddy_ui.dart
@@ -0,0 +1,7 @@
+library movie_buddy_ui;
+
+export "src/colors.dart";
+export "src/layout/layout.dart";
+export "src/navigation/navigation.dart";
+export "src/theme.dart";
+export "src/typography/typography.dart";
